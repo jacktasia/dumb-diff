704b3fc1f842db18000caaeae6f9bb2a846e743f
jack <jacktasia@gmail.com>
2017-08-14 01:19:57 -0700

    basics working

--- dumb-diff.el
+++ dumb-diff.el
@@ -1,19 +1,52 @@
 
+(defvar dumb-diff-f1 nil)
+(defvar dumb-diff-f2 nil)
+(defvar dumb-diff-f-result nil)
+
 (defun dumb-diff-go ()
   (interactive)
-  (delete-other-windows)
+  ;; (let
 
+  (when dumb-diff-f1
+    (kill-buffer (get-buffer dumb-diff-f1)))
+
+  (when dumb-diff-f2
+    (kill-buffer (get-buffer dumb-diff-f2)))
+
+  (when dumb-diff-f-result
+    (kill-buffer (get-buffer dumb-diff-f-result)))
+
+  (setq dumb-diff-f1 "/tmp/dumb-diff-f1")
+  (setq dumb-diff-f2 "/tmp/dumb-diff-f2")
+  (setq dumb-diff-f-result "/tmp/dumb-diff-f-result.diff")
+
+  (delete-other-windows)
   (split-window-below)
   (split-window-right)
   ;;
 
-  (find-file (make-temp-file "DUMB_DIFF_1_"))
+  (find-file dumb-diff-f1)
   (other-window 1)
-  (find-file (make-temp-file "DUMB_DIFF_2_"))
+  (find-file dumb-diff-f2)
   (other-window 1)
-  (find-file (make-temp-file "DUMB_DIFF_RESULT_"))
+  (find-file dumb-diff-f-result)
+  (dumb-diff-refresh))
 
   ;; https://emacs.stackexchange.com/questions/12459/save-buffer-at-each-modification
 
-  (diff-mode)
-  (insert "+123\n-456\n"))
+(defun dumb-diff-refresh ()
+  (interactive)
+  (with-current-buffer (find-file-noselect dumb-diff-f1)
+    (funcall 'basic-save-buffer))
+
+  (with-current-buffer (find-file-noselect dumb-diff-f2)
+    (funcall 'basic-save-buffer))
+
+  (let ((cmd (format "diff -u %s %s > %s" dumb-diff-f1 dumb-diff-f2 dumb-diff-f-result)))
+        (shell-command-to-string cmd)
+
+        (with-current-buffer dumb-diff-f-result
+          (revert-buffer nil t))))
+
+
+;;END
