a9611586509311af16444e515ca4b2936856571f
jack <jacktasia@gmail.com>
2017-08-14 23:45:35 -0700

    use buffers instead of direct files

--- dumb-diff.el
+++ dumb-diff.el
@@ -1,52 +1,54 @@
 
+;; TODO: make major-mode
+
 (defvar dumb-diff-f1 nil)
 (defvar dumb-diff-f2 nil)
 (defvar dumb-diff-f-result nil)
 
 (defun dumb-diff-go ()
   (interactive)
-  ;; (let
 
-  (when dumb-diff-f1
-    (kill-buffer (get-buffer dumb-diff-f1)))
-
-  (when dumb-diff-f2
-    (kill-buffer (get-buffer dumb-diff-f2)))
-
-  (when dumb-diff-f-result
-    (kill-buffer (get-buffer dumb-diff-f-result)))
-
-  (setq dumb-diff-f1 "/tmp/dumb-diff-f1")
-  (setq dumb-diff-f2 "/tmp/dumb-diff-f2")
-  (setq dumb-diff-f-result "/tmp/dumb-diff-f-result.diff")
+  (setq dumb-diff-f1 (get-buffer-create "*Dumb Diff 1*"))
+  (setq dumb-diff-f2 (get-buffer-create "*Dumb Diff 2*"))
+  (setq dumb-diff-f-result (get-buffer-create "*Dumb Diff Result*"))
 
   (delete-other-windows)
   (split-window-below)
   (split-window-right)
-  ;;
 
-  (find-file dumb-diff-f1)
+  (switch-to-buffer dumb-diff-f1)
   (other-window 1)
-  (find-file dumb-diff-f2)
+
+  (switch-to-buffer dumb-diff-f2)
   (other-window 1)
-  (find-file dumb-diff-f-result)
+
+  (switch-to-buffer dumb-diff-f-result)
   (dumb-diff-refresh))
 
-  ;; https://emacs.stackexchange.com/questions/12459/save-buffer-at-each-modification
+(defun dumb-diff-get-buffer-contents (b)
+  (with-current-buffer b
+    (buffer-string)))
+
+(defun dumb-diff-write-to-file (f c)
+  (with-temp-file f
+    (insert
+     (dumb-diff-get-buffer-contents c))))
 
 (defun dumb-diff-refresh ()
   (interactive)
-  (with-current-buffer (find-file-noselect dumb-diff-f1)
-    (funcall 'basic-save-buffer))
 
-  (with-current-buffer (find-file-noselect dumb-diff-f2)
-    (funcall 'basic-save-buffer))
+  (let* ((dd-f1 (make-temp-file "dumb-diff-f1"))
+         (dd-f2 (make-temp-file "dumb-diff-f2")))
 
-  (let ((cmd (format "diff -u %s %s > %s" dumb-diff-f1 dumb-diff-f2 dumb-diff-f-result)))
-        (shell-command-to-string cmd)
+    (dumb-diff-write-to-file dd-f1 dumb-diff-f1)
+    (dumb-diff-write-to-file dd-f2 dumb-diff-f2)
 
-        (with-current-buffer dumb-diff-f-result
-          (revert-buffer nil t))))
+    (let* ((cmd (format "diff -u %s %s" dd-f1 dd-f2))
+           (result (shell-command-to-string cmd)))
+      (with-current-buffer dumb-diff-f-result
+        (funcall 'diff-mode)
+        (erase-buffer)
+        (insert result)))))
 
 
 ;;END
