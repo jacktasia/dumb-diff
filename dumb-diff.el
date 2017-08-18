9f23d958597dac31617614772fdac4730d61b039
jack <jacktasia@gmail.com>
2017-08-17 22:35:55 -0700

    clean up

--- dumb-diff.el
+++ dumb-diff.el
@@ -4,41 +4,36 @@
 ;; TODO: package-ify
 ;; TODO: submit to MELPA
 
-(defvar dumb-diff-f1 nil)
-(defvar dumb-diff-f2 nil)
-(defvar dumb-diff-f-result nil)
+(defvar dumb-diff-buf1 nil)
+(defvar dumb-diff-buf2 nil)
+(defvar dumb-diff-buf-result nil)
+
+(defvar dumb-diff-msg-empty "Press `C-c C-c` to view the diff for the buffers above")
+(defvar dumb-diff-msg-no-difference "(no difference)")
 
 (defun dumb-diff-go ()
   (interactive)
 
-  (setq dumb-diff-f1 (get-buffer-create "*Dumb Diff 1*"))
-  (setq dumb-diff-f2 (get-buffer-create "*Dumb Diff 2*"))
-  (setq dumb-diff-f-result (get-buffer-create "*Dumb Diff Result*"))
+  (setq dumb-diff-buf1 (get-buffer-create "*Dumb Diff 1*"))
+  (setq dumb-diff-buf2 (get-buffer-create "*Dumb Diff 2*"))
+  (setq dumb-diff-buf-result (get-buffer-create "*Dumb Diff Result*"))
 
   (delete-other-windows)
   (split-window-below)
   (split-window-right)
 
-  (switch-to-buffer dumb-diff-f1)
+  (switch-to-buffer dumb-diff-buf1)
   (other-window 1)
 
-  (switch-to-buffer dumb-diff-f2)
+  (switch-to-buffer dumb-diff-buf2)
   (other-window 1)
 
-  (switch-to-buffer dumb-diff-f-result)
+  (switch-to-buffer dumb-diff-buf-result)
   (dumb-diff--refresh))
-
-(defun dumb-diff-select-f1 ()
-  (interactive)
-  (switch-to-buffer dumb-diff-f1))
-
-(defun dumb-diff-select-f2 ()
-  (interactive)
-  (switch-to-buffer dumb-diff-f2))
 
 (defun dumb-diff-select-result ()
   (interactive)
-  (switch-to-buffer dumb-diff-f-result))
+  (switch-to-buffer dumb-diff-buf-result))
 
 (defun dumb-diff-get-buffer-contents (b)
   (with-current-buffer b
@@ -46,29 +41,41 @@
 
 (defun dumb-diff-write-to-file (f c)
   (with-temp-file f
-    (insert
-     (dumb-diff-get-buffer-contents c))))
+    (insert c)))
+
 
 (defun dumb-diff--refresh ()
-  (let* ((dd-f1 (make-temp-file "dumb-diff-f1"))
-         (dd-f2 (make-temp-file "dumb-diff-f2")))
+  "Run `diff` command and update result buffer."
+  (let* ((dd-f1 (make-temp-file "dumb-diff-buf1"))
+         (dd-f2 (make-temp-file "dumb-diff-buf2"))
+         (txt-f1 (dumb-diff-get-buffer-contents dumb-diff-buf1))
+         (txt-f2 (dumb-diff-get-buffer-contents dumb-diff-buf2))
+         (is-blank (and (= (length txt-f1) 0)
+                        (= (length txt-f2) 0))))
 
-    (dumb-diff-write-to-file dd-f1 dumb-diff-f1)
-    (dumb-diff-write-to-file dd-f2 dumb-diff-f2)
+    ;; write buffer contents to temp file for comparison
+    (dumb-diff-write-to-file dd-f1 txt-f1)
+    (dumb-diff-write-to-file dd-f2 txt-f2)
 
-    (with-current-buffer dumb-diff-f1
-      (funcall 'dumb-diff-mode))
-
-    (with-current-buffer dumb-diff-f2
-      (funcall 'dumb-diff-mode))
+    ;; ensure compare buffers have major mode enabled
+    (dolist (x (list dumb-diff-buf2 dumb-diff-buf1))
+      (with-current-buffer x
+        (funcall 'dumb-diff-mode)))
 
     (let* ((cmd (format "diff -u %s %s" dd-f1 dd-f2))
            (raw-result (shell-command-to-string cmd))
-           (result (if (> (length raw-result) 0) raw-result "(no difference)")))
-      (with-current-buffer dumb-diff-f-result
+           (result (if (> (length raw-result) 0)
+                       raw-result
+                     (if is-blank
+                         dumb-diff-msg-empty
+                       dumb-diff-msg-no-difference))))
+
+      ;; TODO: replace tmp file name with the pretty buffer name
+      (with-current-buffer dumb-diff-buf-result
         (funcall 'diff-mode)
         (erase-buffer)
-        (insert result)))))
+        (insert result)))
+    (dumb-diff-select-result)))
 
 
 (defvar dumb-diff-mode-map (make-sparse-keymap)
@@ -76,9 +83,6 @@
 
 (defun dumb-diff-mode-keymap ()
   "Define keymap for `dumb-diff-mode'."
-  (define-key dumb-diff-mode-map (kbd "C-c 1") 'dumb-diff-select-f1) ;; TODO: find better key bindings
-  (define-key dumb-diff-mode-map (kbd "C-c 2") 'dumb-diff-select-f2)
-  (define-key dumb-diff-mode-map (kbd "C-c 3") 'dumb-diff-select-result)
   (define-key dumb-diff-mode-map (kbd "C-c C-c") 'dumb-diff-go))
 
 
