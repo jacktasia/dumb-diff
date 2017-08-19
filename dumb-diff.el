765091049a2e134c2bf292446feeb2e58a8aa916
jack <jacktasia@gmail.com>
2017-08-18 19:59:28 -0700

    replace tmpfile paths with pretty buffer names in diff output

--- dumb-diff.el
+++ dumb-diff.el
@@ -3,10 +3,15 @@
 ;; TODO: add license
 ;; TODO: package-ify
 ;; TODO: submit to MELPA
+;; TODO: make `defcustom`s
+;; TODO: add tests
+;; TODO: add `defcustom` for `dumb-diff-bin-path` (default: "diff") and `dumb-diff-bin-args` (default: "-u")
 
 (defvar dumb-diff-buf1 nil)
 (defvar dumb-diff-buf2 nil)
 (defvar dumb-diff-buf-result nil)
+(defvar dumb-diff-buf1-name "*Dumb Diff 1*")
+(defvar dumb-diff-buf2-name "*Dumb Diff 2*")
 
 (defvar dumb-diff-msg-empty "Press `C-c C-c` to view the diff for the buffers above")
 (defvar dumb-diff-msg-no-difference "(no difference)")
@@ -14,8 +19,8 @@
 (defun dumb-diff-go ()
   (interactive)
 
-  (setq dumb-diff-buf1 (get-buffer-create "*Dumb Diff 1*"))
-  (setq dumb-diff-buf2 (get-buffer-create "*Dumb Diff 2*"))
+  (setq dumb-diff-buf1 (get-buffer-create dumb-diff-buf1-name))
+  (setq dumb-diff-buf2 (get-buffer-create dumb-diff-buf2-name))
   (setq dumb-diff-buf-result (get-buffer-create "*Dumb Diff Result*"))
 
   (delete-other-windows)
@@ -44,6 +49,9 @@
     (insert c)))
 
 
+(defun dumb-diff-string-replace (old new str)
+  (replace-regexp-in-string (regexp-quote old) new str nil 'literal))
+
 (defun dumb-diff--refresh ()
   "Run `diff` command and update result buffer."
   (let* ((dd-f1 (make-temp-file "dumb-diff-buf1"))
@@ -63,14 +71,15 @@
         (funcall 'dumb-diff-mode)))
 
     (let* ((cmd (format "diff -u %s %s" dd-f1 dd-f2))
-           (raw-result (shell-command-to-string cmd))
-           (result (if (> (length raw-result) 0)
-                       raw-result
+           (raw-result1 (shell-command-to-string cmd))
+           (raw-result2 (dumb-diff-string-replace dd-f1 dumb-diff-buf1-name raw-result1))
+           (raw-result3 (dumb-diff-string-replace dd-f2 dumb-diff-buf2-name raw-result2))
+           (result (if (> (length raw-result3) 0)
+                       raw-result3
                      (if is-blank
                          dumb-diff-msg-empty
                        dumb-diff-msg-no-difference))))
 
-      ;; TODO: replace tmp file name with the pretty buffer name
       (with-current-buffer dumb-diff-buf-result
         (funcall 'diff-mode)
         (erase-buffer)
