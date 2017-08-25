ecbb4a48f83054a6dc16c67ed3bfe9a9c7d45986
jack <jacktasia@gmail.com>
2017-08-24 18:08:41 -0700

    add `test-dumb-diff-write-to-file` test

--- test/dumb-diff-test.el
+++ test/dumb-diff-test.el
@@ -8,3 +8,14 @@
     (with-current-buffer buf
       (insert contents))
   (should (string= contents (dumb-diff-get-buffer-contents buf)))))
+
+
+(ert-deftest test-dumb-diff-write-to-file ()
+  (let* ((test-file (make-temp-file "dumb-diff-test-write"))
+         (contents "some-test-contents"))
+  (dumb-diff-write-to-file test-file contents)
+
+  (should (string= contents
+                   (with-temp-buffer
+                     (insert-file-contents test-file)
+                     (buffer-string))))))
