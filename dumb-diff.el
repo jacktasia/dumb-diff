d08ea5f12cfae6e77d2159227455b4b050a47601
jack <jacktasia@gmail.com>
2017-09-19 09:05:11 -0700

    remove completed TODOs

--- dumb-diff.el
+++ dumb-diff.el
@@ -22,10 +22,6 @@
 
 ;; Dumb Diff is an Emacs "diff to definition" package for fast arbitrary diffs.
 
-;; TODO: submit to MELPA
-;; TODO: add more tests
-;; TODO: (dumb-diff-clear)
-
 ;;; Code:
 
 (defgroup dumb-diff nil
