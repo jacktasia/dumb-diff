1a2331d283049b71a07c1b06b1e0627a950d55f4
jack <jacktasia@gmail.com>
2017-12-11 13:22:48 -0800

    on git-diff if diff is empty delete diff buffer

--- dumb-diff.el
+++ dumb-diff.el
@@ -143,11 +143,14 @@
          (file-name (car (last (split-string (buffer-file-name) "/"))))
          (buffer-title (concat "Dumb Diff via git on " file-name " | " (format-time-string "%D @ %H:%M:%S"))))
     (shell-command git-diff-cmd buffer-title)
-    (with-current-buffer (get-buffer buffer-title)
-      (diff-mode)
-      (while (re-search-forward dumb-diff-hunk-header-re-unified nil t)
-        (funcall-interactively 'diff-refine-hunk)))
-    (other-buffer buffer-title)))
+    (let ((buf (get-buffer buffer-title)))
+      (with-current-buffer buf
+          (diff-mode)
+          (switch-to-buffer-other-window buf)
+          (while (re-search-forward dumb-diff-hunk-header-re-unified nil t)
+            (funcall-interactively 'diff-refine-hunk))
+          (when (= (buffer-size) 0)
+            (kill-buffer buf))))))
 
 (defun dumb-diff-set-buffer-by-name (name start end)
   "Injected into buffer NAME the string from region START to END."
